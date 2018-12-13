d8214ca6aab205ad15c11520f4824f1e93704e7c
LeTanThanh <tanthanhcr95@gmail.com>
2018-12-13 18:22:06 +0700

    tmp_dbr

---
+++ app/controllers/scan_controller.rb
@@ -0,0 +1,10 @@
+class ScanController < ApplicationController
+  def idex
+  end
+
+  def dbr
+  end
+
+  def dbr_preview
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "scan#idex"
+
+  get "/dbr", to: "scan#dbr"
+  get "/dbr_preview", to: "scan#dbr_preview"
 end
